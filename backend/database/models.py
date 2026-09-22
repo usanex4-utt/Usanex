aebcecac1cd8268a997163b7f9c4b8760e796e0b
usanex4-utt <usanex4@gmail.com>
2026-09-22 16:51:19 +0530

    Update models.py

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,1 +1,38 @@
+from sqlalchemy import Column, Integer, String
 
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    user_id = Column(
+        String(30),
+        unique=True,
+        index=True,
+        nullable=False,
+    )
+
+    name = Column(
+        String(100),
+        nullable=False,
+    )
+
+    mobile = Column(
+        String(20),
+        unique=True,
+        index=True,
+        nullable=False,
+    )
+
+    password_hash = Column(
+        String(255),
+        nullable=False,
+    )
+
+    profile_photo = Column(
+        String(500),
+        nullable=True,
+    )
